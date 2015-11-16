c0c28c435391f9c43dc1909a632390c52ff1e210
Brett Smith <jbsmith731@gmail.com>
2015-11-15 22:08:55 -0600

    add navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,19 @@ var scroll = (function($) {
 
 	var init = function() {
 
-
+		// Animate body on load
 	  setTimeout(function() {
 	    $('body').addClass('animate-in');
 	  }, 500);
+
+	  // Set waypoints
+	  $('.header-waypoint').waypoint(function(direction) { 
+			$('header').toggleClass('header-fixed', direction === 'down');
+		}, { offset:'500px' });
+
+		$('.waypoint').waypoint(function(direction) {
+      $(this).toggleClass($(this).data('class'), direction === 'down');
+    }, {offset: '40%' });
 
 
 	};
