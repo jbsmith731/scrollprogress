42f74346f4ecea5b673cfbc09abc1e26529791d4
Brett Smith <jbsmith731@gmail.com>
2015-11-16 20:03:48 -0600

    remove inline styles and add to <head>

--- scrollProgress.js
+++ scrollProgress.js
@@ -2,27 +2,20 @@
 
   $.fn.scrollProgress = function( options ) {
 
-    // Add progress div to html
-    $('body').prepend('<div id="progress"></div>');
-
     // Set default settings
     var settings = $.extend({
-      height: 4,
-      backgroundColor: "rgba(246,36,89, 0.75)",
-      opacity: '',
-      offset: '',
+      height          : 4,
+      backgroundColor : "rgba(246,36,89, 0.75)",
+      opacity         : null,
+      offset          : null
     }, options);
 
+     // Add progress div to html
+    $('body').prepend('<div id="progress"></div>');
+
     // Set default CSS options
-    $('#progress').css({
-      height: settings.height,
-      'background-color': settings.backgroundColor,
-      opacity: settings.opacity,
-      position: 'fixed',
-      top: 0,
-      left: 0,
-    })
-    
+    $('head').append('<style type="text/css">#progress { height:' + settings.height + 'px; background-color:' + settings.backgroundColor + ';' + (settings.opacity != null ? 'opacity:' + settings.opacity + ';': '') + ' position: fixed; top: 0; left: 0 } </style>');
+
     // Scroll function
     $(window).on('load scroll resize', function() {
 
